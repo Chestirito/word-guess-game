b891680990aa644f778374ca0634aa49f8724871
Chestirito <19494641+Chestirito@users.noreply.github.com>
2018-06-09 03:20:16 -0400

    aesthetic changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
-var words = ["inuyasha", "digimon", "pokemon", "medabots"];
+var words = ["inuyasha", "digimon", "pokemon", "medabots", "trigun", "bebop", "ranma"];
+//var words = ["dragon balls"];
 var cPick = words[Math.floor(Math.random() * words.length)];
 var wCondition = 0;
 var letterGuessed = [];
@@ -7,9 +8,9 @@ var guessR = document.getElementById("guessRem");
 var lCondition = 12;
 var wins = document.getElementById("wins");
 var tLines = document.getElementById("lines");
-
+var lettersTag = document.getElementById("gWord");
 function resetLines(){
-    var lettersTag = document.getElementById("gWord");
+    //var lettersTag = document.getElementById("gWord");
     //var linesTag = document.getElementById("lines");
     lettersTag.textContent = "";
     //linesTag.textContent = "";
@@ -65,7 +66,7 @@ document.onkeyup = function(event) {
                     //console.log(wCondition);
                 }
             }
-        }//if guess is incorrect
+        }//if guess is incorrect decrease remaining guesses
         else if(!letterGuessed.includes(userGuess)){
             lCondition -= 1;
             guessR.textContent = lCondition;
@@ -80,15 +81,35 @@ document.onkeyup = function(event) {
         
         //if remaining guess reaches 0
         if(lCondition === 0){
-            wins.textContent = 0;
-            resetGame();
+            
+            var interv = setInterval(function() {
+                guessR.style.visibility = (guessR.style.visibility == 'hidden' ? '' : 'hidden');
+            }, 100);
+            //sleep(1000);
+            setTimeout(function( ) { 
+                clearInterval( interv ); 
+                guessR.style.visibility = "";
+                wins.textContent = 0;
+                resetGame();
+            }, 500);
+            
         }
 
         
         //determine win condition
         if(wCondition === cPick.length){
-            wins.textContent = parseInt(wins.textContent) + 1;
-            resetGame();
+            //var f = document.getElementById('Foo');
+            var interv = setInterval(function() {
+                lettersTag.style.visibility = (lettersTag.style.visibility == 'hidden' ? '' : 'hidden');
+            }, 100);
+            //sleep(1000);
+            setTimeout(function( ) { 
+                clearInterval( interv ); 
+                lettersTag.style.visibility = "";
+                wins.textContent = parseInt(wins.textContent) + 1;
+                resetGame();
+            }, 500);
+            
         }
     }
     
